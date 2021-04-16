f42649151b8d02a77e91ed4bae2ea056ffcdec7a
Andrei Savu <andrei@moonsense.io>
2021-04-15 22:25:55 -0700

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='moonsense',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests*']),
     description='Moonsense Cloud API client',
     long_description=long_description,
