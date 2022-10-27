860d79041a9927cfb4589bc0e89857b2ef8ebb03
Bogdan Gaza <bc.gaza@gmail.com>
2022-10-26 21:17:18 -0700

    [python-sdk] Add a logic for CLI tool.

--- moonsense/cli/__init__.py
+++ moonsense/cli/__init__.py
@@ -1,5 +1,5 @@
 import click
-from download import run_download
+from .download import run_download
 
 @click.group()
 def main():
@@ -9,10 +9,12 @@ def main():
 @click.option('--until', help='The date in YYYY-MM-DD format until the data should be included')
 @click.option('--since', help='The date in YYYY-MM-DD format since data should be included')
 @click.option('--label', '-l', multiple=True, help='Labels to filter sessions by')
-def download_cmd():
-    click.echo('Download')
+@click.option('--with-group-id', default=False, help='If true, the folder structure uses includes the \
+        client session group id')
+def download(until, since, label, with_group_id):
+    click.echo('Download', until, since, label, with_group_id)
 
-main.add_command(download_cmd)
+main.add_command(download)
 
 if __name__ == '__main__':
     main()
