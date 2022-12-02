3e89181aa430945e20e838645cc0d9029f923bd5
Jerry <11030825+jnsimon@users.noreply.github.com>
2022-12-01 19:25:13 -0500

    Added methods to get session featurs and signals (#22)
    Co-authored-by: Bogdan Gaza <hurrycane@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="moonsense",
-    version="0.9.2",
+    version="0.10.0",
     packages=find_packages(exclude=["tests*"]),
     description="Moonsense Cloud API Client",
     long_description=long_description,
