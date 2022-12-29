500f42b0e98426826a29beff53e10618a79d1c34
Andrei Savu <andrei@moonsense.io>
2022-12-28 16:15:12 -0800

    Update demo consumer script for fetch signals and features

--- consumer_example.py
+++ consumer_example.py
@@ -22,7 +22,7 @@ On start, the scripts checks for active sessions that received new data in
 the last 90 seconds and for each stars a thread to process chunks.
 
 Each thread maintains a list of processed chunks and for every new chunk
-it will call the process_chunk() method. That method gets access to all the
+it will call the process_bundles_chunk() method. That method gets access to all the
 bundles in a chunk and can publish cards in the session that will be
 visible in the Recorder app.
 """
@@ -37,28 +37,51 @@ from datetime import datetime, timedelta, timezone
 from moonsense.models import Session, Chunk
 from moonsense.client import Client
 
-# Override the process_chunk method to do useful things with the session data
-def process_chunk(client: Client, session: Session, chunk: Chunk) -> None:
+#
+# Signals combine multiple features derived from source data into a single
+# value that can be used to detect suspicious activity. A signal can either
+# be based on rules or on a machine learning model.
+#
+def process_signals(client: Client, session: Session) -> None:
+    try:
+        signals = client.list_session_signals(session.session_id)
+        logging.info(f"Session {session.session_id} signals: {signals}")
+    except Exception as e:
+        pass
+
+
+#
+# Features are derived from the raw data and can be used as input into
+# custom roles or specialized risk scoring models.
+#
+def process_features(client: Client, session: Session) -> None:
+    try:
+        response = client.list_session_features(session.session_id)
+        logging.info(f"Session {session.session_id} has {len(response.features)} features.")
+    except Exception as e:
+        pass
+
+
+#
+# Bundles are the smallest unit of data that can be processed. They are
+# automatically assembled into chunks for efficient processing.
+#
+def process_bundles_chunk(client: Client, session: Session, chunk: Chunk) -> None:
     bundles = list(client.read_chunk(session.session_id, chunk.chunk_id))
-
     logging.info(f"Chunk {chunk.chunk_id} has {len(bundles)} bundles")
-
-    # client.create_card(
-    #     session.session_id,
-    #     "Python Consumer Example",
-    #     f"Chunk {chunk.chunk_id} has {len(bundles)} bundles",
-    # )
 
 
 def main():
     client = Client()
     monitored = set()
+
+    logging.info("Checking for recent sessions that are actively receiving data")
+
     while True:
-        logging.info("Checking for recent sessions that are actively receiving data")
 
         for session in itertools.islice(client.list_sessions(), 25):
             if session.session_id not in monitored and active(session):
-                logging.info("Starting to monitor %s", session.session_id)
+                logging.info("Monitoring %s", session.session_id)
                 monitored.add(session.session_id)
                 threading.Thread(
                     target=worker,
@@ -67,33 +90,32 @@ def main():
                         session,
                     ),
                 ).start()
-
-        logging.info("Waiting for 5 seconds until the next check")
-        time.sleep(5)
+        time.sleep(2)
 
 
 def worker(client: Client, session: Session) -> None:
-    logging.info(
-        "Fetching the initial list of session data chunks for session %s",
-        session.session_id,
-    )
+    time.sleep(5)
+
     processed = set(c.chunk_id for c in client.list_chunks(session.session_id))
+
+    process_signals(client, session)
+    process_features(client, session)
+
     while active(session):
         for chunk in client.list_chunks(session.session_id):
             if chunk.chunk_id not in processed:
                 logging.info(
-                    "Processing chunk %s for session %s",
+                    "Processing bundles chunk %s for session %s",
                     chunk.chunk_id,
                     session.session_id,
                 )
-                process_chunk(client, session, chunk)
+                process_bundles_chunk(client, session, chunk)
                 processed.add(chunk.chunk_id)
 
-        logging.info("Waiting for 30 seconds to check for new session data chunks")
         time.sleep(30)
         session = client.describe_session(session.session_id)
 
-    logging.info("Done monitoring session %s", session.session_id)
+    logging.info("Session %s is inactive", session.session_id)
 
 
 def active(session: Session) -> bool:
@@ -102,5 +124,5 @@ def active(session: Session) -> bool:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     main()
